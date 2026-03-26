3731fb655eb6160c2f1891c0d7d8dbfb877fa0e1
lnx645 <dadanhidyt@gmail.com>
2026-03-26 23:42:21 +0700

    first

--- backend/src/core/server.ts
+++ backend/src/core/server.ts
@@ -1,12 +1,18 @@
 import routes from "@/routes/routes";
 import { serve } from "bun";
+
+type WSData = {
+  secret: string|null;
+  token: string|null;
+  path:string|null
+};
 declare global {
-   var server : Bun.Server<undefined>
+  var server: Bun.Server<WSData>;
 }
 export class Server {
   server: Bun.Server<undefined> | any;
   async RunServer() {
-    let serv = serve({
+    let serv = serve<WSData>({
       hostname: process.env.APP_HOST,
       port: process.env.APP_PORT,
       idleTimeout: 10,
@@ -14,24 +20,32 @@ export class Server {
       fetch(req, server) {
         let url = new URL(req.url);
         if (url.pathname.startsWith(process.env.WEBSOCKET_PATH)) {
-          server.upgrade(req);
+          server.upgrade(req, {
+            data : {
+              secret: url.searchParams.get('secret'),
+              token : url.searchParams.get('token'),
+              path : url.pathname,
+            },
+          });
           return;
         }
         return new Response("WebSocket upgrade failed", { status: 500 });
       },
       websocket: {
         open(ws) {
-          ws.subscribe("chat:91");
+          console.log(ws.data);
+          
+          ws.subscribe(ws.data.token!);
         },
         message: function (ws) {
-           ws.publish("chat:91","Jalan")
+          ws.publish("chat:91", "Jalan");
         },
         close(ws, code, reason) {
-            ws.send("DIsconnected")
+          ws.unsubscribe(ws.data.token!);
         },
       },
     });
-    globalThis.server = serv
+    globalThis.server = serv;
     console.log("🌐 Server running at: 3000");
   }
 }
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,7 +2,7 @@ import html from "../../view/index.html";
 export default {
   "/html" : html,
   "/": (req: Bun.BunRequest<"/">) => {
-    globalThis.server.publish("chat:91", "Berhasil jalan di webserver1192");
+    globalThis.server.publish("098234324", "Berhasil jalan di webserver1192");
     return Response.json({
       status: "OKE",
     });
